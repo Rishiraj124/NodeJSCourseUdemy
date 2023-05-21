c0a2375ab5beea9334bf9a15830c0b8b46d4bf58
RishiDevGit <rishirajsaxena@makewebbetter.com>
2023-05-21 17:01:35 +0530

    node js changes

---
+++ app.js
@@ -0,0 +1,5 @@
+const http=require("http");
+const server=http.createServer(function(req,res) {
+    //if the request comes execute this function
+});
+server.listen(3000);  //web server which keeps listening to incoming requests actively
